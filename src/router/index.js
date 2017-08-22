077ff8ef2aa91a612160e1cd9c3a28a024006aaf
jxinchao <425775961@qq.com>
2017-08-22 18:30:51 +0800

    默认显示路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,12 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   linkActiveClass: 'active', // 设置tab切换的类名
   routes: [
-    { path: '/', component: goods },
     { path: '/goods', component: goods },
     { path: '/ratings', component: ratings },
     { path: '/seller', component: seller }
   ]
 });
+
+router.push('/goods');  // 将goods添加到 进入默认显示的路由
 
 export default router;
